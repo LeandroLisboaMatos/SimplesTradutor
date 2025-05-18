87124292f50e715b9d4f555592bef2b409b0e393
LeandroLisboaMatos <leandro.matos@discente.ufma>
2025-05-18 10:42:19 -0300

    LendoNumero+1Digito

--- src/Scanner.java
+++ src/Scanner.java
@@ -1,43 +1,53 @@
 public class Scanner {
 
     private byte[] input;
-    private int current; 
+    private int current;
 
-	public Scanner (byte[] input) {
+    public Scanner(byte[] input) {
         this.input = input;
     }
 
-    private char peek () {
+    private char peek() {
         if (current < input.length)
-           return (char)input[current];
-       return '\0';
+            return (char) input[current];
+        return '\0';
     }
 
-        private void advance()  {
+    private void advance() {
         char ch = peek();
         if (ch != '\0') {
             current++;
         }
     }
 
-    public char nextToken () {
+    public String nextToken() {
         char ch = peek();
-
-        if (Character.isDigit(ch)) {
-						advance();
-            return ch;
-        }
+        if (ch == '0') {
+            advance();
+            return Character.toString(ch);
+        } else if (Character.isDigit(ch))
+            return number();
 
         switch (ch) {
             case '+':
             case '-':
                 advance();
-                return ch;
+                return Character.toString(ch);
             default:
                 break;
         }
 
-        return '\0';
+        throw new Error("lexical error");
+    }
+
+    private String number() {
+        int start = current;
+        while (Character.isDigit(peek())) {
+            advance();
+        }
+
+        String n = new String(input, start, current - start);
+        return n;
     }
 
 }
